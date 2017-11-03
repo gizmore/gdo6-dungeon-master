81826cb5b01ee75d73c4a39dc3e5b514d096f95e
gizmore <gizmore@wechall.net>
2017-11-03 02:27:46 +0100

    6.06_gdt_menu

---
+++ GDT_DMRace.php
@@ -0,0 +1,9 @@
+<?php
+namespace GDO\DungeonMaster;
+
+use GDO\DB\GDT_Enum;
+
+final class GDT_DMRace extends GDT_Enum
+{
+	
+}
---
+++ Module_DungeonMaster.php
@@ -0,0 +1,9 @@
+<?php
+namespace GDO\DungeonMaster;
+
+use GDO\Core\GDO_Module;
+
+final class Module_DungeonMaster extends GDO_Module
+{
+	
+}
